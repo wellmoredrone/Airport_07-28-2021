7d602ec152462393226637b8200d5953fe75c999
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2021-08-01 23:51:34 -0400

    init

---
+++ data.js
@@ -0,0 +1,379 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overall-view",
+      "name": "Overall View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.3626344344136747,
+        "pitch": 0.27801403899520594,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1383834034912876,
+          "pitch": 0.06732722241575217,
+          "rotation": 0,
+          "target": "1-blair-1-pit"
+        },
+        {
+          "yaw": 0.6471295537821682,
+          "pitch": 0.14881215586038898,
+          "rotation": 0,
+          "target": "2-eagle-1-pit"
+        },
+        {
+          "yaw": 3.0048141449913217,
+          "pitch": 0.0353962064527682,
+          "rotation": 0,
+          "target": "3-durable-rock-fill---2"
+        },
+        {
+          "yaw": 2.14457343740615,
+          "pitch": 0.03734856841685108,
+          "rotation": 0,
+          "target": "5-durable-rock-fill---3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15570367847498545,
+          "pitch": 0.06506981641579834,
+          "title": "Eagle Development",
+          "text": "Development yards for future pits."
+        },
+        {
+          "yaw": -1.3569291922287618,
+          "pitch": 0.15037306309369924,
+          "title": "Blair 2 Development",
+          "text": "Development yards/blasting for future Blair 2 pits."
+        },
+        {
+          "yaw": -2.256393855024033,
+          "pitch": 0.209011920313932,
+          "title": "Durable Rock Fill - 1",
+          "text": "DRF - 1 is in the early stages of fill. No terraces are completed at this time.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-blair-1-pit",
+      "name": "Blair 1 Pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.20906965200591188,
+        "pitch": 0.1527803307192599,
+        "fov": 1.289016311897305
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07836458017518311,
+          "pitch": -0.12507361908843606,
+          "rotation": 0,
+          "target": "0-overall-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3103310329927211,
+          "pitch": 0.06694130426137335,
+          "title": "Mine Office",
+          "text": "Location of the mine office."
+        },
+        {
+          "yaw": 0.3710816151145373,
+          "pitch": 0.07639448264465543,
+          "title": "Blair 2 Development",
+          "text": "Development yards/blasting for future Blair 2 pits."
+        }
+      ]
+    },
+    {
+      "id": "2-eagle-1-pit",
+      "name": "Eagle 1 Pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.13103131539064172,
+        "pitch": 0.06939689576944552,
+        "fov": 1.1662156583522731
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7567015965768569,
+          "pitch": -0.15037895353935404,
+          "rotation": 0,
+          "target": "0-overall-view"
+        },
+        {
+          "yaw": -1.5331004867955862,
+          "pitch": -0.026573059163986557,
+          "rotation": 0,
+          "target": "3-durable-rock-fill---2"
+        },
+        {
+          "yaw": -2.087509868831056,
+          "pitch": -0.02014550311192309,
+          "rotation": 0,
+          "target": "5-durable-rock-fill---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-durable-rock-fill---2",
+      "name": "Durable Rock Fill - 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.002745084718203117,
+        "pitch": 0.5732371743960982,
+        "fov": 1.3551843124310574
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.058600863730260855,
+          "pitch": 0.011684982845814318,
+          "rotation": 0,
+          "target": "0-overall-view"
+        },
+        {
+          "yaw": 0.012878613209512224,
+          "pitch": 1.1509014006787908,
+          "rotation": 0,
+          "target": "4-durable-rock-fill---2-closeup"
+        },
+        {
+          "yaw": 0.5525650109632743,
+          "pitch": 0.012587973978888556,
+          "rotation": 0,
+          "target": "5-durable-rock-fill---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-durable-rock-fill---2-closeup",
+      "name": "Durable Rock Fill - 2 Closeup",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.11510618138732553,
+        "pitch": 0.054907569822875146,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1600101831644345,
+          "pitch": -0.24378065232019708,
+          "rotation": 0,
+          "target": "0-overall-view"
+        },
+        {
+          "yaw": -0.01226329789850844,
+          "pitch": -0.5773302079242999,
+          "rotation": 0,
+          "target": "3-durable-rock-fill---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-durable-rock-fill---3",
+      "name": "Durable Rock Fill - 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.0694959102304118,
+        "pitch": 0.6740596526975082,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.935307938983426,
+          "pitch": -0.015654039191812075,
+          "rotation": 0,
+          "target": "2-eagle-1-pit"
+        },
+        {
+          "yaw": 0.6198789065740495,
+          "pitch": -0.05343441610417443,
+          "rotation": 0,
+          "target": "0-overall-view"
+        },
+        {
+          "yaw": -0.623745347992406,
+          "pitch": -0.04832416709672316,
+          "rotation": 0,
+          "target": "3-durable-rock-fill---2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Airport Surface 07-28-2021",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
